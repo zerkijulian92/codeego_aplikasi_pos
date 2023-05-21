501bd8782005879bdbeab295f545104d9272129a
zerkijulian92 <zerkijuliandri92@gmail.com>
2023-05-21 15:29:46 +0700

    Menambahkan kode insert.php

--- produk/insert.php
+++ produk/insert.php
@@ -1,1 +1,14 @@
-a
+<h3>INSERT DATA PRODUK</h3>
+<form action="" method="post" name="form1">
+    <input type="text" name="kodeproduk" placeholder="Kode Produk"><br>
+    <select name="kategori">
+        <option value="satu">Satu</option>
+        <option value="dua">Dua</option>
+    </select><br>
+    <input type="text" name="namaproduk" placeholder="Nama Produk"><br>
+    <input type="text" name="hargadasar" placeholder="Harga Dasar"><br>
+    <input type="text" name="hargajual" placeholder="Harga Jual"><br>
+    <input type="text" name="stok" placeholder="Stok Barang"><br>
+    <button type="submit">Simpan</button>
+    <input type="hidden" name="MM_insert" value="form1">
+</form>
