adac87a13b2687b7870bd32c4a96013851ab57ec
zerkijulian92 <zerkijuliandri92@gmail.com>
2023-06-05 17:09:05 +0700

    Menambahkan file dasboard.php

--- login.php
+++ login.php
@@ -1,3 +1,58 @@
+<!-- Memanggil Fungsi Koneksi.php -->
+<?php require_once('koneksi/koneksi.php'); ?>
+
+<?php
+// *** Validate request to login to this site.
+if (!isset($_SESSION)) {
+    session_start();
+    if (isset($_SESSION['MM_Username'])) {
+        $halaman = "dashboard.php";
+        header("Location: " . $halaman);
+        exit;
+    }
+}
+
+$loginFormAction = $_SERVER['PHP_SELF'];
+if (isset($_GET['accesscheck'])) {
+    $_SESSION['PrevUrl'] = $_GET['accesscheck'];
+}
+
+if (isset($_POST['username'])) {
+    $loginUsername = $_POST['username'];
+    $password = $_POST['password'];
+    $MM_fldUserAuthorization = "";
+    $MM_redirectLoginSuccess = "dashboard.php";
+    $MM_redirectLoginFailed = "login.php";
+    $MM_redirecttoReferrer = true;
+    mysqli_select_db($koneksi, $database_koneksi);
+
+    $LoginRS__query = sprintf(
+        "SELECT userkassa, pwdkassa FROM kassa WHERE userkassa=%s AND pwdkassa=%s",
+        inj($koneksi, $loginUsername, "text"),
+        inj($koneksi, $password, "text")
+    );
+
+    $LoginRS = mysqli_query($koneksi, $LoginRS__query) or die(mysqli_error($koneksi));
+    $loginFoundUser = mysqli_num_rows($LoginRS);
+    if ($loginFoundUser) {
+        $loginStrGroup = "";
+
+        //declare two session variables and assign them
+        $_SESSION['MM_Username'] = $loginUsername;
+        $_SESSION['MM_UserGroup'] = $loginStrGroup;
+
+        if (isset($_SESSION['PrevUrl']) && true) {
+            $MM_redirectLoginSuccess = $_SESSION['PrevUrl'];
+        }
+        header("Location: " . $MM_redirectLoginSuccess);
+    } else {
+        header("Location: " . $MM_redirectLoginFailed);
+    }
+}
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,7 +61,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Login</title>
-    <!-- Fungsi Menampilkan fungsi checkbox pada halaman login -->
+    <!-- Fungsi Menampilkan checkbox hide password pada halaman login -->
     <script>
         function togglePasswordVisibility() {
             var passwordInput = document.getElementById("password");
@@ -23,9 +78,9 @@
 
 <body>
     <h1>Silahkan Login</h1>
-    <form action="" method="pos">
+    <form action="<?php echo $loginFormAction; ?>" method="post" name="login">
         <label for="username">Username</label><br>
-        <input type="text" name="" id="username">
+        <input type="text" name="username" id="username">
         <br>
         <label for="password">Password</label><br>
         <input type="password" name="password" id="password">
