23b71dd03475d574fa60fa9fc2c1dbad6d233da6
zerkijulian92 <zerkijuliandri92@gmail.com>
2023-06-13 22:40:02 +0700

    Edit direktori kategori

--- kategori/read.php
+++ kategori/read.php
@@ -1,6 +1,6 @@
 <?php
 // menghubungkan ke file koneksi.php
-require_once('../koneksi/koneksi.php');
+require_once('koneksi/koneksi.php');
 // MENAMBAHKAN FUNGSI CARI
 $cari = "-1";
 if (isset($_GET['cari'])) {
@@ -21,11 +21,12 @@ $totalRows = mysqli_num_rows($eksekusi);
 ?>
 
 <h3>DAFTAR KATEGORI</h3>
-<a href="insert.php">Kategori</a>
+<a href="?page=kategori/insert">Kategori</a>
 <p></p>
 <form action="" method="get">
     Cari Data : <input type="text" name="cari">
     <button type="submit">Search</button>
+    <input type="hidden" name="page" value="kategori/read">
 </form>
 
 <!-- MENAMBAHKAN FUNGSI KETIKA DATA TIDAK TERSEDIA(KOSONG) -->
@@ -45,7 +46,7 @@ $totalRows = mysqli_num_rows($eksekusi);
                 <td><?php echo $no; ?></td>
                 <td><?php echo $row['namakategori']; ?></td>
                 <td><?php echo $row['ketkategori']; ?></td>
-                <td><a href="update.php?id=<?php echo $row['idkategori']; ?>"> Edit</a> | <a href="delete.php?id=<?php echo $row['idkategori']; ?>">Hapus</td>
+                <td><a href="?page=kategori/update&id=<?php echo $row['idkategori']; ?>"> Edit</a> | <a href="?page=kategori/delete&id=<?php echo $row['idkategori']; ?>">Hapus</td>
             </tr>
         <?php
             $no++;
--- kategori/update.php
+++ kategori/update.php
@@ -1,41 +1,43 @@
-<?php 
-    require_once('../koneksi/koneksi.php');
+<?php
+require_once('koneksi/koneksi.php');
 
-    // EDIT DATA
-    $editFormAction = $_SERVER['PHP_SELF'];
-        if (isset($_SERVER['QUERY_STRING'])) {
-        $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
-    }
+// EDIT DATA
+$editFormAction = $_SERVER['PHP_SELF'];
+if (isset($_SERVER['QUERY_STRING'])) {
+    $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+}
 
 
-    if ((isset($_POST["MM_update"])) && ($_POST["MM_update"] == "form1")) {
-        $updateSQL = sprintf("UPDATE kategori SET namakategori=%s, ketkategori=%s WHERE idkategori=%s",
-                        inj($koneksi, $_POST['namakategori'], "text"),
-	                    inj($koneksi, $_POST['ketkategori'], "text"),
-	                    inj($koneksi, $_POST['idkategori'], "int"));
-        $Result1 = mysqli_query($koneksi, $updateSQL) or die(errorQuery(mysqli_error($koneksi)));
+if ((isset($_POST["MM_update"])) && ($_POST["MM_update"] == "form1")) {
+    $updateSQL = sprintf(
+        "UPDATE kategori SET namakategori=%s, ketkategori=%s WHERE idkategori=%s",
+        inj($koneksi, $_POST['namakategori'], "text"),
+        inj($koneksi, $_POST['ketkategori'], "text"),
+        inj($koneksi, $_POST['idkategori'], "int")
+    );
+    $Result1 = mysqli_query($koneksi, $updateSQL) or die(errorQuery(mysqli_error($koneksi)));
 }
 // EDIT DATA============================================
 
-    $id = "-1";
-    if (isset($_GET['id'])){
-        $id = $_GET['id'];
-    }
+$id = "-1";
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+}
 
-    $query = sprintf("SELECT  * FROM kategori WHERE idkategori = %s ORDER BY idkategori ASC", inj($koneksi, $id, "int"));
-    $eksekusi = mysqli_query($koneksi, $query) or die(errorQuery(mysqli_error($koneksi)));
-    $row = mysqli_fetch_assoc($eksekusi);
-    $totalRows = mysqli_num_rows($eksekusi);
+$query = sprintf("SELECT  * FROM kategori WHERE idkategori = %s ORDER BY idkategori ASC", inj($koneksi, $id, "int"));
+$eksekusi = mysqli_query($koneksi, $query) or die(errorQuery(mysqli_error($koneksi)));
+$row = mysqli_fetch_assoc($eksekusi);
+$totalRows = mysqli_num_rows($eksekusi);
 
 
 ?>
 
 <h3>UPDATE DATA KATEGORI</h3>
 <form action="<?php echo $editFormAction; ?>" name="form1" method="POST">
-   <input type="text" name="namakategori" value="<?php echo htmlentities($row['namakategori'], ENT_COMPAT, '') ; ?>"><br>
-   <input type="text" name="ketkategori" value="<?php echo htmlentities($row['ketkategori'], ENT_COMPAT, '') ; ?>"><br>
-   <button type="submit">SIMPAN</button><a href="read.php">Kembali</a>
-   <input type="hidden" name="MM_update" value="form1"><br>
-   <input type="hidden" name="idkategori" value="<?php echo htmlentities($row['idkategori'], ENT_COMPAT, '') ; ?>"><br>
+    <input type="text" name="namakategori" value="<?php echo htmlentities($row['namakategori'], ENT_COMPAT, ''); ?>"><br>
+    <input type="text" name="ketkategori" value="<?php echo htmlentities($row['ketkategori'], ENT_COMPAT, ''); ?>"><br>
+    <button type="submit">SIMPAN</button><a href="?page=kategori/read">Kembali</a>
+    <input type="hidden" name="MM_update" value="form1"><br>
+    <input type="hidden" name="idkategori" value="<?php echo htmlentities($row['idkategori'], ENT_COMPAT, ''); ?>"><br>
 
 </form>
