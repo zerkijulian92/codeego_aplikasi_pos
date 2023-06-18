23d20ee44a8f68ad02cd1cf504c4f91fc414eb4e
zerkijulian92 <zerkijuliandri92@gmail.com>
2023-06-18 20:54:53 +0700

    Menambahkan fungsi trigger pada database dan edit file scan.php

--- transaksi/scan.php
+++ transaksi/scan.php
@@ -97,6 +97,41 @@ $totalRowsTemp = mysqli_num_rows($eksekusiTemp);
 
 ?>
 
+<!-- Insert Data Transaksi -->
+<?php
+$editFormAction = $_SERVER['PHP_SELF'];
+if (isset($_SERVER['QUERY_STRING'])) {
+    $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_POST["MM_selesai"])) && ($_POST["MM_selesai"] == "selesai")) {
+    $insertSQL = "INSERT INTO `transaksi`(`faktur`, `tanggal`, `produk`, `nama_produk`, `harga`, `harga_dasar`, `qty`, `potongan`, `kassa`, `nama_kassa`, `periode`) VALUES";
+    foreach ($eksekusiTemp as $data) {
+        $insertSQL .= '(' . inj($koneksi, $data['faktur'], "text") . ','
+            . inj($koneksi, $data['tanggal'], "date") . ','
+            . inj($koneksi, $data['produk'], "text") . ','
+            . inj($koneksi, $data['nama_produk'], "text") . ','
+            . inj($koneksi, $data['harga'], "double") . ','
+            . inj($koneksi, $data['harga_dasar'], "double") . ','
+            . inj($koneksi, $data['qty'], "int") . ','
+            . inj($koneksi, $data['potongan'], "double") . ','
+            . inj($koneksi, $data['kassa'], "int") . ','
+            . inj($koneksi, $data['nama_kassa'], "text") . ','
+            . inj($koneksi, $data['periode'], "text") . '),';
+    }
+    $a = substr_replace($insertSQL, ";", -1); //Berfungsi untuk menganti koma di akhir menjdi titik koma
+    $Result1 = mysqli_query($koneksi, $a) or die(errorQuery(mysqli_error($koneksi)));
+
+    if ($Result1) {
+        echo "<script>
+            alert('Transaksi berhasil dilakukan!');
+            document.location = '?page=transaksi/scan';
+        </script>";
+    }
+}
+?>
+<!--  -->
+
 <h3>SCAN HERE</h3>
 <form action="" method="post">
     <label for="cari">Scan Barcode</label>
@@ -137,6 +172,13 @@ $totalRowsTemp = mysqli_num_rows($eksekusiTemp);
         } while ($rowTemp = mysqli_fetch_assoc($eksekusiTemp)); ?>
     </table>
     <H3>Total Belanja : Rp. <?php echo number_format($total); ?></H3>
+
+    <!-- Menambahkan Button selesai -->
+    <form action="<?php echo $editFormAction; ?>" method="post" name="selesai">
+        <button type="submit">Selesai</button>
+        <input type="hidden" name="MM_selesai" value="selesai">
+    </form>
+
 <?php } else { ?>
     Keranjang masih kosong
 <?php } ?>
